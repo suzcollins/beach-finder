da3e085cfa1a6f935e41a1e8300eb9029dfd516a
FatemahZaidi <fzzaidi123@gmail.com>
2019-08-02 10:20:56 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 500;
+var h1 = document.querySelector('.titleone');
+var p = document.querySelector('.pone');
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+
+typeEffect(h1, speed);
+
+
+// type affect to body
+setTimeout(function(){
+  p.style.display = "inline-block";
+  typeEffect(p, speed);
+}, delay);
